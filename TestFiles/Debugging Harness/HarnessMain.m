4e013686359f7a11371e16919f292eb91c249ebb
Ben Gottlieb <ben@standalone.com>
2009-02-10 12:15:39 +0800

    added harness for testing the whitelist window, added option to add to whitelist, had to brutalize current bindings, there's probably a better way to do it, but my binding-fu is weak.
    Signed-off-by: Jonathan 'Wolf' Rentzsch <jwr.git@redshed.net>

---
+++ TestFiles/Debugging Harness/HarnessMain.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Testing
+//
+//  Created by Ben Gottlieb on 2/9/09.
+//  Copyright Stand Alone, Inc. 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
