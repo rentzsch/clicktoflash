8bae1fdef48614eb27616121ff47cdf93d12b181
rentzsch <jwr.git@redshed.net>
2009-03-10 01:39:57 -0500

    [DEV] Abandon our CFBundleVersion fix-up since our preupgrade script is only run *after* Installer.app decides it won't perform the installation (because a newer version (700) exists).

--- Package/scripts/fix_CFBundleVersion.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-if File.exist?("#{ENV['HOME']}/Library/Internet Plug-Ins/ClickToFlash.plugin/Contents/Info.plist")
-  bundle_version = `defaults read "$HOME/Library/Internet Plug-Ins/ClickToFlash.plugin/Contents/Info" CFBundleVersion`.chomp
-  if bundle_version == "700"
-    short_version_string = `defaults read "$HOME/Library/Internet Plug-Ins/ClickToFlash.plugin/Contents/Info" CFBundleShortVersionString`.chomp
-    `defaults write "$HOME/Library/Internet Plug-Ins/ClickToFlash.plugin/Contents/Info" CFBundleVersion #{short_version_string}`
-  end
-end
