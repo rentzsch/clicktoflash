194f2dc1a8da91298d2e79fb426b60ecf4205d2a
Simone Manganelli <simmy@DN800cbe0c.Stanford.EDU>
2009-03-10 17:57:05 +0800

    added buttons in settings panel to allow for manual update checking
    Signed-off-by: Jonathan 'Wolf' Rentzsch <jwr.git@redshed.net>

--- Plugin/CTFWhitelistWindowController.m
+++ Plugin/CTFWhitelistWindowController.m
@@ -1,4 +1,7 @@
 #import "CTFWhitelistWindowController.h"
+#import <Sparkle/Sparkle.h>
+
+NSString *kCTFCheckForUpdates = @"CTFCheckForUpdates";
 
 @implementation CTFWhitelistWindowController
 
@@ -14,6 +17,19 @@
     self = [super initWithWindowNibPath: nibPath owner: self];
 
     return self;
+}
+
+- (IBAction)checkForUpdates:(id)sender;
+{
+	// this code is put here, because if it's code that's owned by the plugin object, then initiating
+	// an update will silently fail when no ClickToFlash view is loaded; putting it in the whitelist window
+	// object allows Sparkle to always check for updates
+	
+	NSBundle *clickToFlashBundle = [NSBundle bundleWithIdentifier:@"com.github.rentzsch.clicktoflash"];
+	NSAssert(clickToFlashBundle, nil);
+	SUUpdater *updater = [SUUpdater updaterForBundle:clickToFlashBundle];
+	NSAssert(updater, nil);
+	[updater checkForUpdates:self];
 }
 
 @end
