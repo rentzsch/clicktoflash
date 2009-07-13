99ffac04dd510d2dd1ecedb194f4733dbb346306
Simone Manganelli <simmy@DNab422ad1.Stanford.EDU>
2009-07-14 07:20:35 +0800

    put setting up of extra menu items back on to the main thread
    Signed-off-by: Jonathan 'Wolf' Rentzsch <jwr.git@redshed.net>

--- Plugin/Plugin.m
+++ Plugin/Plugin.m
@@ -1089,7 +1089,9 @@ BOOL usingMATrackingArea = NO;
 		// menu before the H.264 variants were determined, so we need to
 		// make sure these features are made available to them
 		
-		[self setUpExtraMenuItems];
+		[self performSelectorOnMainThread:@selector(setUpExtraMenuItems)
+							   withObject:nil
+							waitUntilDone:NO];
 	}
 }
 
