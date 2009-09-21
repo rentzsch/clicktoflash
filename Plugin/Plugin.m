75d0e40614d9c3f71900863b37ce6e3fd2e7acdd
Simone Manganelli <simX@mac.com>
2009-09-21 03:39:00 -0700

    reduced the timeout for H.264 variant checking for whitelisted sites back down to 3 from 10

--- Plugin/Plugin.m
+++ Plugin/Plugin.m
@@ -359,7 +359,7 @@ BOOL usingMATrackingArea = NO;
 				
 				// the _checkForH264VideoVariants method will manually fire
 				// this timer if it finishes before the 3 seconds are up
-				_delayingTimer = [NSTimer scheduledTimerWithTimeInterval:10
+				_delayingTimer = [NSTimer scheduledTimerWithTimeInterval:3
 																  target:self
 																selector:@selector(_convertTypesForContainer)
 																userInfo:nil
